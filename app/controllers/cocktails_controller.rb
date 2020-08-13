c4a5c9b3250bb43838247e8234f9f696bd6eda64
Arthur Bizet <arthur.m.bizet@gmail.com>
2020-08-13 18:01:51 +0200

    yes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,11 +13,17 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-   if @cocktail.save
+    if @cocktail.save
       redirect_to @cocktail, notice: 'Restaurant was successfully created.'
     else
       render :new
     end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
